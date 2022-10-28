b4cf9d6768aef006952d07a53d11c44a64f4c617
Sam Flower <66141971+flowerco@users.noreply.github.com>
2022-10-28 10:51:36 +0100

    Added: config file

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+  dbHost: process.env.DB_HOST
+    ? process.env.DB_HOST
+    : 'localhost',
+
+  dbName: process.env.DB_NAME
+    ? process.env.DB_NAME
+    : 'okane',
+
+  dbPort: process.env.DB_PORT
+    ? process.env.DB_PORT
+    : 5432,
+
+  serverPort: process.env.SERVER_PORT
+  ? process.env.SERVER_PORT
+  : 3001, 
+}
+
+export default config;
